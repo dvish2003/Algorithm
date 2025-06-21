f7f342f322bb79e1672457aba4b2e7dc6f03cba1
vishan <Vishanchathuranga2003@gmail.com>
2025-06-22 03:26:10 +0530

    reverse to array list in java and python

---
+++ Algorithm_Q2.java
@@ -0,0 +1,15 @@
+public class Algorithm_Q2 {
+    public static void main(String[] args) {
+        
+int[] numList = {232,23,12,432,33,12,435,12,5};
+int[] numList_1 = new int[numList.length];
+
+   for (int i = 0; i < numList.length; i++) {
+    numList_1[i] = numList[(numList.length-1)-i];
+
+   }
+
+    }
+}
+
+// reverse array list
---
+++ Algorithm_Q2.py
@@ -0,0 +1,10 @@
+numList = [121,32,3,43,5,63,17,8]
+
+numList_2 = reversed(numList) 
+
+
+for num in numList[::-1]:
+	print(num)
+  
+  
+  #reverse array 
